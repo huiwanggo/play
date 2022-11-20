01bf6cf7e0bb45db6a817b2f00aab7769e6dc1ed
Huiwang <huiwanggo@gmail.com>
2022-11-20 22:49:42 +0800

    redis test

--- test/redis_test.go
+++ test/redis_test.go
@@ -5,14 +5,27 @@ import (
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"testing"
-	"time"
 )
 
 func TestConn(t *testing.T) {
+	ctx := context.Background()
 	rdb := redis.NewClusterClient(&redis.ClusterOptions{
 		Addrs: []string{":7001", ":7002", ":7003", ":7004", ":7005", ":7006"},
 	})
 
-	rdb.Set(context.Background(), "test", "test", time.Hour)
-	fmt.Println(rdb.Get(context.Background(), "test").Result())
+	result, err := rdb.ClusterNodes(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+
+	// ForEachShard ForEachMaster ForEachSlave
+	err = rdb.ForEachShard(ctx, func(ctx context.Context, client *redis.Client) error {
+		s, err := client.Ping(ctx).Result()
+		fmt.Println(s)
+		return err
+	})
+	if err != nil {
+		panic(err)
+	}
 }
